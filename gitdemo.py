f37c0eb6a2979a3f856b12aa7c20a6585f503689
bmbasit <bmboffice2020@gmail.com>
2023-01-04 14:48:08 +0530

    new committed

--- gitdemo.py
+++ gitdemo.py
@@ -2,8 +2,5 @@ print("Hello Everyone!!!")
 print("BMB")
 print("Line 3 by me")
 print("Line 3 by BM")
-<<<<<<< HEAD
-print("line 5 local")
-=======
 print("line 5 by repo")
->>>>>>> 29eb66e02527b791009bb398428efac62db54d51
+print("iii")
